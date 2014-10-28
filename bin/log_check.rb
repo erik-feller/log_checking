b544c236c9485353810641ca4f9a4668d0bf80fa
erik-feller <erik.feller@colorado.edu>
2014-10-28 13:13:22 -0600

    Added file for main log parsing code module.

---
+++ bin/log_check.rb
@@ -0,0 +1,13 @@
+#ruby script to load and chunk log files for use with various matcher extensions. 
+#Configurable through ../etc/config
+#Extensions should be in ../bin and have their config files in ../etc/
+#Version 0.00
+#################################################################################
+#To Do
+#Add ability to skip old logs
+#Add ability to give first run flag so that the whole log isn't read.
+#Add ability to have single and multi line running modes. 
+#Standard format for input into the extension functions. 
+#Done
+#Nothing
+
