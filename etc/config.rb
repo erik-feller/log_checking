b9fe012b16cbfb862cd41fa72bb7255d202685af
erik-feller <erik.feller@colorado.edu>
2014-10-28 15:24:32 -0600

    Added file that holds the configuration settings

---
+++ etc/config.rb
@@ -0,0 +1,14 @@
+#Configuration file for log parsing and matching
+
+#Define maximum window size to look at at one time
+MAX_WIN_SIZE = 10**8
+
+#define matchers and matcher location 
+MATCHERLOC = '../bin/' #Must be given a relative location. 
+MATCHERS = ['example1.rb'] #Class defined in these files should have the same name as the file itself
+
+#define logs and log location
+#Might change this mechanic. All the logs might not be in the same spot
+LOGLOC = '/var/logs/'
+LOGS = ['sudo.log']
+
