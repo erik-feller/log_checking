e4dc3843489d062d853db0cc26cc5f9ab646ff44
erik-feller <erik.feller@colorado.edu>
2015-02-12 09:28:13 +0000

    Finished update function

--- lib/log.class.rb
+++ lib/log.class.rb
@@ -1,12 +1,12 @@
 #!/usr/local/bin/ruby
-#This defines a class to describe logs that need to be checked.
-#Will include functions to determine whether or not a log is new. 
-#Going to add some YAML support.
-#I have used a SHA-2 256 bit hash if too slow reduce to an md5sum however this hash best avoids collisions.
-#Calling to system has injection vulnerabilities... not sure how else to do it though.
+#This file describes the Log class which is responsible for maintaining whether or not a log has new content
+#The class is responsible for updating the offset accordingly so that each log can be treated as ready for parsing
+#Also defines a function that can be used to serialize and deserialize data for each log to be saved between runtimes
+#Anything else it needs to do...
 
 require 'digest'
 require 'yaml'
+maxchunk = 1000000
 
 class Log
 	#Gotta init each instance, cksum and old_offset start at 0 because thats how we know not to attack the whole file
@@ -19,6 +19,18 @@ class Log
 		@matchers = ""
 	end
 
+	#Function to determine if log has new content or not. This can help to avoid running update unnecessarily
+	det is_new?
+		sum = Digest::SHA256.new
+		newfile = File.read(@location)
+		sum.update newfile
+		if sum == @cksum
+			return true
+		else
+			return false
+		end
+	end
+	
 	#Now we want to come up with a function to determine if the content of the log has changed 
 	def update
 		#first checks to see if the cksum is even different for any of the programs 
@@ -37,11 +49,26 @@ class Log
 				#This will be done by building a checksum from the part of the old file behind the new one. 
 				offset = 0
 				newfile = File.open(@location)
-					
+				sum = Digest::SHA256.new
+				while offset < @oldoffset
+					chunk = newfile.sysread(maxchunk)
+					sum.update chunk
+					offset += maxchunk
+				end
+				if sum == @cksum
+					return
+				else
+					@oldoffset = 0
+					return
+				end
+			end			
 		end
-
-
 	end
+	def serialize
+	end
+	def loadfs
+	end
+	
 end	
 
 
