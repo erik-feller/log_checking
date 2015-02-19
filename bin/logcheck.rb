8d5d723916eef9d85c3e449f0dd3bb10e4bd2590
erik-feller <erik.feller@colorado.edu>
2015-02-19 15:01:58 +0000

    Adding the main file

---
+++ bin/logcheck.rb
@@ -0,0 +1,19 @@
+#*********************************************************************************************************
+#File which relies on the log and matcher classes to parse throught the given logs and search for certain patterns.
+#Returns a Nagios compatible value once it has read the logs using the matchers.
+#
+
+require 'yaml'
+require 'Digest'
+require_relative '../lib/log.class.rb'
+require_relative '../lib/matcher.class.rb'
+require_relative '../etc/config.rb'
+
+#First thing to happen needs to be adding requires for the logs and matchers that we need. Old code can help us with this.
+
+
+#Now we need to load in the hash containing all of the serialized log objects. 
+#Check to see if any new logs are in the list that aren't included in the hash. If there are any initialize them and add them to the hash. 
+#Now simply walk through all of the logs given and run the matchers that are associated with them. If a matcher cannot be found return an error. Probably 3. 
+#Given the matcher exists then you can parse the log from @old_offset to the EOF in chunks. This program should be in charge of handling parsing either by line or 20 lines or whatever scheme the matcher wants. 
+#Matchers return values to this program. This program will return values to nagios.
